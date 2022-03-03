8892267640b13738d4d29dcac16428bccf754eac
“mdreu” <mdreusi@gmail.com>
2022-03-03 10:06:02 -0500

    Homework

---
+++ script.js
@@ -0,0 +1,13 @@
+let warmHugs = "I'm Olaf and I like warm hugs." 
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs);
+let beenImpaled = "Oh, look at that. I've been impaled.";
+beenImpaled = beenImpaled.slice(18,36);
+console.log(beenImpaled);
+let dotDotDot = "...";
+let skullBones = "I don't have a skull " + dotDotDot + "or bones.";
+console.log(Math.PI);
+let randomNumber = Math.round(1 + (Math.random() * 3));
+console.log(randomNumber);
